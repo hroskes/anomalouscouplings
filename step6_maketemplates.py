5496fb0dad528fa3f2d9f88f7ec42c69347ba705
Heshy Roskes <jroskes1@jhu.edu>
2016-06-08 15:49:50 +0200

    Script to make controlplots

--- step6_maketemplates.py
+++ step6_maketemplates.py
@@ -8,7 +8,7 @@ def buildtemplates(flavor):
     print flavor
     if os.path.exists(flavor.templatesfile()):
         return
-    subprocess.call([os.path.join(config.repositorydir, "TemplateBuilder/buildTemplate.exe"), "step5_json/templates_{}.json".format(channel)])
+    subprocess.call([os.path.join(config.repositorydir, "TemplateBuilder/buildTemplate.exe"), "step5_json/templates_{}.json".format(flavor)])
     if not os.path.exists(flavor.templatesfile()):
         raise RuntimeError("Something is wrong!  {} was not created.".format(flavor.templatesfile()))
 
---
+++ step7_templates/makecontrolplots.py
@@ -0,0 +1,27 @@
+from helperstuff.enums import Channel
+import os
+import ROOT
+import subprocess
+
+def makecontrolplots(flavor):
+    flavor = Channel(flavor)
+    f = ROOT.TFile(flavor.templatesfile())
+    d = f.controlPlots
+
+    split = os.path.split(flavor.templatesfile())
+    saveasdir = os.path.join(split[0], "controlplots_"+split[1].replace("_fa3Adap_new.root", ""))
+    try:
+        os.mkdir(saveasdir)
+    except OSError:
+        pass
+
+    exts = "png", "eps", "root", "pdf"
+
+    for key in d.GetListOfKeys():
+      for ext in exts:
+        key.ReadObj().SaveAs("{}/{}.{}".format(saveasdir, key.ReadObj().GetName(), ext))
+
+if __name__ == "__main__":
+    makecontrolplots("4e")
+    makecontrolplots("4mu")
+    makecontrolplots("2e2mu")
