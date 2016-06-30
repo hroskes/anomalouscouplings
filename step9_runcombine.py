34dc9ed3cec9006d220d24c874a0b8dfd7f759f4
Heshy Roskes <jroskes1@jhu.edu>
2016-06-30 22:33:04 +0200

    channel by channel, update CombinedLimit

--- step9_runcombine.py
+++ step9_runcombine.py
@@ -2,7 +2,7 @@ from Alignment.OfflineValidation.TkAlAllInOneTool.helperFunctions import replace
 from helperstuff import config
 from helperstuff import filemanager
 from helperstuff.combinehelpers import getrates
-from helperstuff.enums import Analysis, channels
+from helperstuff.enums import Analysis, Channel, channels
 from helperstuff.plotlimits import plotlimits
 import os
 import pipes
@@ -16,17 +16,26 @@ python make_prop_DCsandWSs.py -i SM_inputs_8TeV -a .oO[foldername]Oo. -A .oO[ana
 
 runcombinetemplate = """
 eval $(scram ru -sh) &&
-combineCards.py hzz4l_2e2muS_8TeV.txt hzz4l_4muS_8TeV.txt hzz4l_4eS_8TeV.txt > hzz4l_4l_8TeV.txt &&
+combineCards.py .oO[cardstocombine]Oo. > hzz4l_4l_8TeV.txt &&
 text2workspace.py -m 125 hzz4l_4l_8TeV.txt -P HiggsAnalysis.CombinedLimit.SpinZeroStructure:spinZeroHiggs --PO=muFloating -o fixedMu_8TeV.root -v 7 &&
 combine -M MultiDimFit fixedMu_8TeV.root --algo=grid --points 100 -m 125 -n $1_8TeV -t -1 --setPhysicsModelParameters CMS_zz4l_fg4=0.0 --expectSignal=1 -V -v 3
 """
 
-def runcombine(analysis, foldername):
+def runcombine(analysis, foldername, **kwargs):
+    usechannels = channels
+    for kw, kwarg in kwargs.iteritems():
+        if kw == "channels":
+            usechannels = [Channel(c) for c in kwarg.split(",")]
+        else:
+            raise ValueError("Unknown kwarg: {}".format(kw))
+
+
     analysis = Analysis(analysis)
     foldername = "{}_{}".format(analysis, foldername)
     repmap = {
               "foldername": pipes.quote(foldername),
               "analysis": str(analysis),
+              "cardstocombine": " ".join("hzz4l_{}S_8TeV.txt".format(channel) for channel in usechannels)
              }
     with filemanager.cd(os.path.join(config.repositorydir, "CMSSW_7_6_5/src/HiggsAnalysis/HZZ4l_Combination/CreateDatacards")):
         if not os.path.exists("cards_{}".format(foldername)):
@@ -36,14 +45,18 @@ def runcombine(analysis, foldername):
         with filemanager.cd("cards_{}/HCG/125".format(foldername)):
             #replace rates
             for channel in channels:
-                with open("hzz4l_{}S_8TeV.txt".format(channel)) as f:
-                    contents = f.read()
-                for line in contents.split("\n"):
-                    if line.startswith("rate"):
-                        contents = contents.replace(line, "#"+line+"\n"+getrates(channel, "fa2"))
-                        break
-                with open("hzz4l_{}S_8TeV.txt".format(channel), "w") as f:
-                     f.write(contents)
+                if channel in usechannels:
+                    with open("hzz4l_{}S_8TeV.txt".format(channel)) as f:
+                        contents = f.read()
+                    for line in contents.split("\n"):
+                        if line.startswith("rate"):
+                            contents = contents.replace(line, "#"+line+"\n"+getrates(channel, "fa2"))
+                            break
+                    with open("hzz4l_{}S_8TeV.txt".format(channel), "w") as f:
+                        f.write(contents)
+                else:
+                    os.remove("hzz4l_{}S_8TeV.txt".format(channel))
+                    os.remove("hzz4l_{}S_8TeV.input.root".format(channel))
             if not os.path.exists("higgsCombine_8TeV.MultiDimFit.mH125.root"):
                 subprocess.check_call(replaceByMap(runcombinetemplate, repmap), shell=True)
             saveasdir = os.path.join(config.plotsbasedir, "limits", foldername)
@@ -56,4 +69,10 @@ def runcombine(analysis, foldername):
 if __name__ == "__main__":
     analysis = Analysis(sys.argv[1])
     foldername = sys.argv[2]
-    runcombine(analysis, foldername)
+    kwargs = {}
+    for arg in sys.argv[3:]:
+        kw, kwarg = arg.split("=")
+        if kw in kwargs:
+            raise TypeError("Duplicate kwarg {}!".format(kw))
+        kwargs[kw] = kwarg
+    runcombine(analysis, foldername, **kwargs)
