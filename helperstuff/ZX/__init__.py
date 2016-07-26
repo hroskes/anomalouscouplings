84b6c88843629df2b837e76ef018673082aac024
Heshy Roskes <jroskes1@jhu.edu>
2016-07-26 14:24:18 +0200

    160725 Z+X

--- helperstuff/ZX/__init__.py
+++ helperstuff/ZX/__init__.py
@@ -11,6 +11,9 @@ def setup(production):
 
 import convertTGraphstoTH1Fs
 convertTGraphstoTH1Fs.convertTGraphstoTH1Fs(os.path.join(ZXfolder, "FakeRate_SS_2016B.root"))
+#https://github.com/CJLST/ZZAnalysis/blob/b4cc949af2ca81a9dd2bfeaba47768c7ea0cfd13/AnalysisStep/data/FakeRates/FakeRate_SS_2016D.root
 convertTGraphstoTH1Fs.convertTGraphstoTH1Fs(os.path.join(ZXfolder, "FakeRate_SS_2016D.root"))
+#https://github.com/CJLST/ZZAnalysis/blob/4b0ac1ccda2a60295e8233069dcd1a17802894f7/AnalysisStep/data/FakeRates/FakeRate_SS_2016D.root
+convertTGraphstoTH1Fs.convertTGraphstoTH1Fs(os.path.join(ZXfolder, "FakeRate_SS_2016D_12.9fb-1.root"))
 
 from ROOT import CRZLLss, fakeRate13TeV, test_bit
