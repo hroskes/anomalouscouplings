ea2196b9b0ad53be175ea08486bbe3d3e6579e6c
Heshy Roskes <jroskes1@jhu.edu>
2016-07-30 00:23:12 +0200

    print a table

--- step7_templates/printrates.py
+++ step7_templates/printrates.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from helperstuff import config
 from helperstuff.combinehelpers import getrates
 from helperstuff.enums import DataTree
@@ -9,12 +10,16 @@ import sys
 
 if __name__ == "__main__":
     totalexp = totalobs = 0
+    exp = Counter()
     for production in config.productionsforcombine:
         print production
+        productionmodes = ("signal", "qqZZ", "ggZZ", "ZX")
         for flavor in "2e2mu", "4e", "4mu":
             print flavor
             print
             rates = getrates(flavor, "fordata", production, *sys.argv[1:])
+            for p, rate in zip(productionmodes, rates.split()[1:]):
+                exp[p,flavor] += float(rate)
             print "{:.2f} {:.2f} {:.2f} {:.2f}".format(*(float(_) for _ in rates.split()[1:]))
             print "Total bkg: {:.2f}".format(sum(float(_) for _ in rates.split()[2:]))
             print "Total expected: {:.2f}".format(sum(float(_) for _ in rates.split()[1:]))
@@ -24,6 +29,21 @@ if __name__ == "__main__":
             print
         print
 
+    print
+    print
+    print "Table:"
+    print
+    flavors = ["4e", "4mu", "2e2mu"]
+    fmt = "{:<10}"*4
+    print fmt.format(*([""]+flavors))
+    totals = Counter()
+    for p in "qqZZ", "ZX", "ggZZ":
+        print fmt.format(*([p] + ["{:.2f}".format(exp[p,f]) for f in flavors]))
+        for f in flavors:
+            totals[f] += exp[p,f]
+    print fmt.format(*(["bkg"] + ["{:.2f}".format(totals[f]) for f in flavors]))
+    print fmt.format(*(["signal"] + ["{:.2f}".format(exp["signal",f]) for f in flavors]))
+    print fmt.format(*(["observed"] + [sum(tfiles[DataTree(production, flavor).treefile].candTree.GetEntries() for production in config.productionsforcombine) for flavor in flavors]))
     print
     print
     print "Total:"
