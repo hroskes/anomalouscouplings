c7149ae3959c6dc22afe9b24809f8bd5076e2d4e
Heshy Roskes <jroskes1@jhu.edu>
2016-09-07 20:26:45 +0200

    ability to scan with different number of points or range

--- step10_plottingutilities/limits.py
+++ step10_plottingutilities/limits.py
@@ -103,13 +103,16 @@ def printlimits(analysis, foldername, **kwargs):
                 repmap["minuscl"] = range_[0] - minimum.x
 
         repmap["95%"] = " \cup ".join("[{:.2g},{:.2g}]".format(range_[0],range_[1]) for range_ in results[3.84])
-        if NLL[1] >= NLL[0]:
-            prob = ROOT.TMath.Prob(NLL[1]-NLL[0], 1)/2
-        else:
-            prob = 1 - ROOT.TMath.Prob(NLL[0]-NLL[1], 1)/2
 
         print printformat.printformat.format(**repmap)
-        print "Probability for pure BSM vs. pure SM: {:.2g}{}".format(prob*100, "%" if printformat == "ppt" else r"\%")
+
+        if 0 in NLL and 1 in NLL:
+            if NLL[1] >= NLL[0]:
+                prob = ROOT.TMath.Prob(NLL[1]-NLL[0], 1)/2
+            else:
+                prob = 1 - ROOT.TMath.Prob(NLL[0]-NLL[1], 1)/2
+
+            print "Probability for pure BSM vs. pure SM: {:.2g}{}".format(prob*100, "%" if printformat == "ppt" else r"\%")
         print
         print
 
