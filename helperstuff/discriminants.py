11b877afe66301b147bda16b88513bedc1cbff92
Heshy Roskes <jroskes1@jhu.edu>
2016-09-07 22:30:07 +0200

    tweak ranges

--- helperstuff/discriminants.py
+++ helperstuff/discriminants.py
@@ -2,6 +2,38 @@ from collections import namedtuple
 
 Discriminant = namedtuple("Discriminant", "name title bins min max")
 
+minmax_g1jgik = {
+    ("g4", 0, ""): (0, 1),
+    ("g4", 4, ""): (0, 1),
+    ("g2", 0, ""): (0, 1),
+    ("g2", 4, ""): (0, 1),
+    ("g1prime2", 0, ""): (0, 1),
+    ("g1prime2", 4, ""): (0, 1),
+
+
+    ("g4", 1, ""): (-10, 10),
+    ("g4", 2, ""): (0, 30),
+    ("g4", 3, ""): (-4, 4),
+    #dummy values
+    ("g2", 1, ""): (-100, 100),
+    ("g2", 2, ""): (0, 100),
+    ("g2", 3, ""): (-100, 100),
+    ("g1prime2", 1, ""): (-100, 100),
+    ("g1prime2", 2, ""): (0, 100),
+    ("g1prime2", 3, ""): (-100, 100),
+
+    ("g4", 1, "_prime"): (-1, 1),
+    ("g4", 2, "_prime"): (0, 1),
+    ("g4", 3, "_prime"): (-.5, .5),
+    #dummy values
+    ("g2", 1, "_prime"): (-1, 1),
+    ("g2", 2, "_prime"): (0, 1),
+    ("g2", 3, "_prime"): (-1, 1),
+    ("g1prime2", 1, "_prime"): (-1, 1),
+    ("g1prime2", 2, "_prime"): (0, 1),
+    ("g1prime2", 3, "_prime"): (-1, 1),
+}
+
 discriminants = {
     d.name: d for d in [
         Discriminant("D_bkg_0plus", "D_{bkg}", 20, 0, 1),
@@ -29,14 +61,18 @@ discriminants = {
                      "D_g1{}_{}{}_VBFdecay{}".format(i, gj, 4-i, prime),
                      "D{}_[g_[1]^[{}]g_[{}]^[{}]]".format("'" if prime else "", i, gj, 4-i).replace("[", "{").replace("]", "}"),
                      20,
-                     0 if i%2==0 else -1,
-                     1
+                     *(
+                            (0 if i%2==0 else -1, 1) if prime
+                       else (minmax_g1jgik[gj, i, prime])
+                      )
                     )
             for prime in ("", "_prime")
             for gj in ("g4", "g2", "g1prime2")
             for i in range(5)
     ]
 }
+
+del minmax_g1jgik
 
 def discriminant(name):
     return discriminants[name]
