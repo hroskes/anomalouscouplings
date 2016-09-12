5b0fcd7989b3f0d58c2d4ac25ce7713e3631f348
Heshy Roskes <jroskes1@jhu.edu>
2016-09-12 20:22:15 +0200

    plotting

--- step10_plottingutilities/plotMus.py
+++ step10_plottingutilities/plotMus.py
@@ -1,40 +1,57 @@
 from ROOT import *
 
-from helperstuff import style
+from helperstuff import config, style
+from collections import namedtuple
+import os
 
 gStyle.SetPadTickY(0)
 gStyle.SetTickLength(0.0, "Y");
 gStyle.SetEndErrorSize(10)
 
 from array import array
+from limits import printlimits
 
-from results import results
-
-a_mus = {}; v_mus = {}; g_mus = {};
+a_mus_up = {}; a_mus_dn = {}; v_mus_up = {}; v_mus_dn = {}; g_mus = {};
 a_y = {}; v_y = {};
 
 a_zero = array('d',[0])
 v_zero = TVectorD(len(a_zero),a_zero)
 
-for lumi in ['300fb','300fb_sc2','3000fb','3000fb_sc2']:
-  i=5
-  for mu in ['r_tot','r_ggH','r_VBF','r_VH','r_ttH']:
+Lumi = namedtuple("Lumi", "lumi foldername fa2plotname fa3plotname fL1plotname")
+lumis = [
+         Lumi("300fb",      "September12_forECFA_300",
+                            "limit_100,-0.05,0.05", "limit_100,-0.2,0.2", "limit_50,-0.2,0.2"),
+         Lumi("300fb_sc2",  "September12_forECFA_300_scenario2",
+                            "limit_100,-0.05,0.05", "limit_100,-0.2,0.2", "limit_50,-0.2,0.2"),
+         Lumi("3000fb",     "September12_forECFA",
+#                            "limit_80,-0.004,0.004", "limit_60,-0.12,0.12", "limit_100,-0.02,0.02"),
+                            "limit_80,-0.004,0.004", "limit_60,-0.12,0.12", "limit_10,-0.02,0.02"),
+         Lumi("3000fb_sc2", "September12_forECFA_scenario2",
+#                            "limit_80,-0.004,0.004", "limit_60,-0.12,0.12", "limit_100,-0.02,0.02"),
+                            "limit_80,-0.004,0.004", "limit_60,-0.12,0.12", "limit_10,-0.02,0.02"),
+        ]
+
+for lumi, foldername, fa2plotname, fa3plotname, fL1plotname in lumis:
+  for i, (mu, plotname) in enumerate(zip(['fa3','fa2','fL1'], (fa3plotname, fa2plotname, fL1plotname)), start=1):
+
+    print lumi, mu
+    minimum, results = printlimits(mu, foldername, plotname=plotname)
+    assert len(results[3.84]) == 1
 
     a_y[lumi+'_'+mu] = array('d',[i])
     v_y[lumi+'_'+mu] = TVectorD(len(a_y[lumi+'_'+mu]),a_y[lumi+'_'+mu])
 
-    a_mus[lumi+'_'+mu] = array('d',[(results[lumi+'_'+mu]['up']/results[lumi+'_'+mu]['central']+
-                                    results[lumi+'_'+mu]['dn']/results[lumi+'_'+mu]['central'])/2] )
-    v_mus[lumi+'_'+mu] = TVectorD(len(a_mus[lumi+'_'+mu]),a_mus[lumi+'_'+mu])
+    a_mus_up[lumi+'_'+mu] = array('d',[ results[3.84][0][1]])
+    a_mus_dn[lumi+'_'+mu] = array('d',[-results[3.84][0][0]])
+    v_mus_up[lumi+'_'+mu] = TVectorD(len(a_mus_up[lumi+'_'+mu]),a_mus_up[lumi+'_'+mu])
+    v_mus_dn[lumi+'_'+mu] = TVectorD(len(a_mus_dn[lumi+'_'+mu]),a_mus_dn[lumi+'_'+mu])
 
-    g_mus[lumi+'_'+mu] = TGraphAsymmErrors(v_zero,v_y[lumi+'_'+mu],v_zero,v_mus[lumi+'_'+mu],v_zero,v_zero)
+    g_mus[lumi+'_'+mu] = TGraphAsymmErrors(v_zero,v_y[lumi+'_'+mu],v_mus_dn[lumi+'_'+mu],v_mus_up[lumi+'_'+mu],v_zero,v_zero)
 
     g_mus[lumi+'_'+mu].SetLineWidth(3)
 
     if ('sc2' in lumi): g_mus[lumi+'_'+mu].SetLineColor(kRed)
     else: g_mus[lumi+'_'+mu].SetLineColor(kGreen+2)
-
-    i-=1
 
 for lumi in ['300fb','3000fb']:
 
@@ -42,8 +59,8 @@ for lumi in ['300fb','3000fb']:
   c1.SetRightMargin(0.05)
   c1.SetLeftMargin(0.05)
 
-  if (lumi=='300fb'): dummy = TH1D("dummy","dummy",1,-0.159,0.879)
-  if (lumi=='3000fb'): dummy = TH1D("dummy","dummy",1,-0.049,0.299)
+  if (lumi=='300fb'): dummy = TH1D("dummy","dummy",1,-0.2,0.2)
+  if (lumi=='3000fb'): dummy = TH1D("dummy","dummy",1,-0.02,0.02)
 
   dummy.SetMinimum(0.0)
   dummy.SetMaximum(7.0)
@@ -67,19 +84,19 @@ for lumi in ['300fb','3000fb']:
   
   latex2.SetTextSize(0.55*c1.GetTopMargin())
   latex2.DrawLatex(0.08,0.85, "Expected uncertainties on")
-  latex2.DrawLatex(0.08,0.81, "Higgs Boson signal strengths")
+  latex2.DrawLatex(0.08,0.81, "Higgs Boson anomalous couplings")
   
   latex2.SetTextSize(0.55*c1.GetTopMargin())
   latex2.DrawLatex(0.65,0.66, "H #rightarrow ZZ* #rightarrow 4#font[12]{l}")
-  latex2.DrawLatex(0.65,0.60, "m_{H} = 125.09 GeV")
+  latex2.DrawLatex(0.65,0.60, "m_{H} = 125 GeV")
   
   latex2.SetTextSize(0.55*c1.GetTopMargin())
   
-  for mu in ['r_tot','r_ggH','r_VBF','r_VH','r_ttH']:
+  for mu in ["fa3", "fa2", "fL1"]:
     g_mus[lumi+'_'+mu].Draw("|same")
     g_mus[lumi+'_sc2_'+mu].Draw("|same")
 
-    latex2.DrawLatex(0.08,a_y[lumi+'_'+mu][0]/8.7+0.13, "#mu_{"+mu.replace("r_","")+"}^{ZZ}")
+    latex2.DrawLatex(0.08,a_y[lumi+'_'+mu][0]/8.7+0.13, mu.replace("f", "f_{").replace("L", "#Lambda")+"}^{ZZ}")
 
   line =  TLine(0.0,0.0, 0.0, 5.5)
   line.SetLineWidth(2)
@@ -91,7 +108,12 @@ for lumi in ['300fb','3000fb']:
   legend.AddEntry(g_mus[lumi+'_'+mu], lumi.replace("fb","")+" fb^{-1} at #sqrt{s}=13 TeV Scenario 1", "l")
   legend.AddEntry(g_mus[lumi+'_sc2_'+mu], lumi.replace("fb","")+" fb^{-1} at #sqrt{s}=13 TeV Scenario 2", "l")
   legend.Draw("same")
-  
-  c1.SaveAs("muPerProc_"+lumi+".pdf")
+
+  saveasdir = os.path.join(config.plotsbasedir, "ECFAsummary")
+  try: os.makedirs(saveasdir)
+  except OSError: pass
+
+  for ext in "png eps root pdf".split():
+    c1.SaveAs(os.path.join(saveasdir, "faisummary_{}.{}".format(lumi, ext)))
   
 
