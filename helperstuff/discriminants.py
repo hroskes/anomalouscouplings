8354524f5a9a892e7a937cc76a7231a3184dca60
Heshy Roskes <jroskes1@jhu.edu>
2016-10-10 20:47:40 +0200

    remove race condition in step 6 (technically there may still be one, but only if running template builder is really fast, and unfortunately it's not)

--- helperstuff/discriminants.py
+++ helperstuff/discriminants.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from config import defaultnbins
 
 Discriminant = namedtuple("Discriminant", "name title bins min max")
 
@@ -66,71 +67,71 @@ minmax_g1jgik = {
 
 discriminants = {
     d.name: d for d in [
-        Discriminant("D_bkg_0plus", "D_{bkg}", 20, 0, 1),
-        Discriminant("D_bkg_0plus_ResUp", "D_{bkg}^{ResUp}", 20, 0, 1),
-        Discriminant("D_bkg_0plus_ResDown", "D_{bkg}^{ResDown}", 20, 0, 1),
-        Discriminant("D_bkg_0plus_ScaleUp", "D_{bkg}^{ScaleUp}", 20, 0, 1),
-        Discriminant("D_bkg_0plus_ScaleDown", "D_{bkg}^{ScaleDown}", 20, 0, 1),
-        Discriminant("D_0minus_decay", "D_{0-}^{dec}", 20, 0, 1),
-        Discriminant("D_CP_decay", "D_{CP}^{dec}", 20, -0.5, 0.5),
-        Discriminant("D_g2_decay", "D_{0h+}^{dec}", 20, 0, 1),
-        Discriminant("D_g1g2_decay", "D_{int}^{dec}", 20, 0, 1),
-        Discriminant("D_g1prime2_decay", "D_{#Lambda1}^{dec}", 20, 0, 1),
-        Discriminant("D_g1g1prime2_decay", "D_{#Lambda1int}^dec", 20, 0, 1),
-        Discriminant("D_0minus_VBF", "D_{0-}^{VBF}", 20, 0, 1),
-        Discriminant("D_CP_VBF", "D_{CP}^{VBF}", 20, -1, 1),
-        Discriminant("D_g2_VBF", "D_{0h+}^{VBF}", 20, 0, 1),
-        Discriminant("D_g1g2_VBF", "D_{int}^{VBF}", 20, -1, 1),
-        Discriminant("D_g1prime2_VBF", "D_{#Lambda1}^{VBF}", 20, 0, 1),
-        Discriminant("D_g1g1prime2_VBF", "D_{#Lambda1int}^{VBF}", 20, -1, 1),
-        Discriminant("D_0minus_VBFdecay", "D_{0-}^{VBFdec}", 20, 0, 1),
-        Discriminant("D_g2_VBFdecay", "D_{0h+}^{VBFdec}", 20, 0, 1),
-        Discriminant("D_g1prime2_VBFdecay", "D_{#Lambda1}^{VBFdec}", 20, 0, 1),
+        Discriminant("D_bkg_0plus", "D_{bkg}", defaultnbins, 0, 1),
+        Discriminant("D_bkg_0plus_ResUp", "D_{bkg}^{ResUp}", defaultnbins, 0, 1),
+        Discriminant("D_bkg_0plus_ResDown", "D_{bkg}^{ResDown}", defaultnbins, 0, 1),
+        Discriminant("D_bkg_0plus_ScaleUp", "D_{bkg}^{ScaleUp}", defaultnbins, 0, 1),
+        Discriminant("D_bkg_0plus_ScaleDown", "D_{bkg}^{ScaleDown}", defaultnbins, 0, 1),
+        Discriminant("D_0minus_decay", "D_{0-}^{dec}", defaultnbins, 0, 1),
+        Discriminant("D_CP_decay", "D_{CP}^{dec}", defaultnbins, -0.5, 0.5),
+        Discriminant("D_g2_decay", "D_{0h+}^{dec}", defaultnbins, 0, 1),
+        Discriminant("D_g1g2_decay", "D_{int}^{dec}", defaultnbins, 0, 1),
+        Discriminant("D_g1prime2_decay", "D_{#Lambda1}^{dec}", defaultnbins, 0, 1),
+        Discriminant("D_g1g1prime2_decay", "D_{#Lambda1int}^dec", defaultnbins, 0, 1),
+        Discriminant("D_0minus_VBF", "D_{0-}^{VBF}", defaultnbins, 0, 1),
+        Discriminant("D_CP_VBF", "D_{CP}^{VBF}", defaultnbins, -1, 1),
+        Discriminant("D_g2_VBF", "D_{0h+}^{VBF}", defaultnbins, 0, 1),
+        Discriminant("D_g1g2_VBF", "D_{int}^{VBF}", defaultnbins, -1, 1),
+        Discriminant("D_g1prime2_VBF", "D_{#Lambda1}^{VBF}", defaultnbins, 0, 1),
+        Discriminant("D_g1g1prime2_VBF", "D_{#Lambda1int}^{VBF}", defaultnbins, -1, 1),
+        Discriminant("D_0minus_VBFdecay", "D_{0-}^{VBFdec}", defaultnbins, 0, 1),
+        Discriminant("D_g2_VBFdecay", "D_{0h+}^{VBFdec}", defaultnbins, 0, 1),
+        Discriminant("D_g1prime2_VBFdecay", "D_{#Lambda1}^{VBFdec}", defaultnbins, 0, 1),
     ] + [
         Discriminant(
                      "D_g1{}_{}{}_VBFdecay{}".format(i, gj, 4-i, prime),
                      "D{}^[VBFdec]_[g_[1]^[{}]g_[{}]^[{}]]".format("'" if prime else "", i, gj, 4-i).replace("[", "{").replace("]", "}"),
-                     20,
+                     defaultnbins,
                      *minmax_g1jgik["VBF", gj, i, prime]
                     )
             for prime in ("", "_prime")
             for gj in ("g4", "g2", "g1prime2")
             for i in range(1, 4)
     ] + [
-        Discriminant("D_0minus_ZH_hadronic", "D_{0-}^{ZHh}", 20, 0, 1),
-        Discriminant("D_CP_ZH_hadronic", "D_{CP}^{ZHh}", 20, -.4, .4),
-        Discriminant("D_g2_ZH_hadronic", "D_{0h+}^{ZHh}", 20, 0, 1),
-        Discriminant("D_g1g2_ZH_hadronic", "D_{int}^{ZHh}", 20, -1, .05),
-        Discriminant("D_g1prime2_ZH_hadronic", "D_{#Lambda1}^{ZHh}", 20, 0, 1),
-        Discriminant("D_g1g1prime2_ZH_hadronic", "D_{#Lambda1int}^{ZHh}", 20, 0, 1),
-        Discriminant("D_0minus_ZHdecay_hadronic", "D_{0-}^{ZHhdec}", 20, 0, 1),
-        Discriminant("D_g2_ZHdecay_hadronic", "D_{0h+}^{ZHhdec}", 20, 0, 1),
-        Discriminant("D_g1prime2_ZHdecay_hadronic", "D_{#Lambda1}^{ZHhdec}", 20, 0, 1),
+        Discriminant("D_0minus_ZH_hadronic", "D_{0-}^{ZHh}", defaultnbins, 0, 1),
+        Discriminant("D_CP_ZH_hadronic", "D_{CP}^{ZHh}", defaultnbins, -.4, .4),
+        Discriminant("D_g2_ZH_hadronic", "D_{0h+}^{ZHh}", defaultnbins, 0, 1),
+        Discriminant("D_g1g2_ZH_hadronic", "D_{int}^{ZHh}", defaultnbins, -1, .05),
+        Discriminant("D_g1prime2_ZH_hadronic", "D_{#Lambda1}^{ZHh}", defaultnbins, 0, 1),
+        Discriminant("D_g1g1prime2_ZH_hadronic", "D_{#Lambda1int}^{ZHh}", defaultnbins, 0, 1),
+        Discriminant("D_0minus_ZHdecay_hadronic", "D_{0-}^{ZHhdec}", defaultnbins, 0, 1),
+        Discriminant("D_g2_ZHdecay_hadronic", "D_{0h+}^{ZHhdec}", defaultnbins, 0, 1),
+        Discriminant("D_g1prime2_ZHdecay_hadronic", "D_{#Lambda1}^{ZHhdec}", defaultnbins, 0, 1),
     ] + [
         Discriminant(
                      "D_g1{}_{}{}_ZHdecay_hadronic{}".format(i, gj, 4-i, prime),
                      "D{}^[ZHhdec]_[g_[1]^[{}]g_[{}]^[{}]]".format("'" if prime else "", i, gj, 4-i).replace("[", "{").replace("]", "}"),
-                     20,
+                     defaultnbins,
                      *minmax_g1jgik["ZHh", gj, i, prime]
                     )
             for prime in ("", "_prime")
             for gj in ("g4", "g2", "g1prime2")
             for i in range(1, 4)
     ] + [
-        Discriminant("D_0minus_WH_hadronic", "D_{0-}^{WHh}", 20, 0, 1),
-        Discriminant("D_CP_WH_hadronic", "D_{CP}^{WHh}", 20, -.1, .1),
-        Discriminant("D_g2_WH_hadronic", "D_{0h+}^{WHh}", 20, 0, 1),
-        Discriminant("D_g1g2_WH_hadronic", "D_{int}^{WHh}", 20, -1, .05),
-        Discriminant("D_g1prime2_WH_hadronic", "D_{#Lambda1}^{WHh}", 20, 0, 1),
-        Discriminant("D_g1g1prime2_WH_hadronic", "D_{#Lambda1int}^{WHh}", 20, 0, 1),
-        Discriminant("D_0minus_WHdecay_hadronic", "D_{0-}^{WHhdec}", 20, 0, 1),
-        Discriminant("D_g2_WHdecay_hadronic", "D_{0h+}^{WHhdec}", 20, 0, 1),
-        Discriminant("D_g1prime2_WHdecay_hadronic", "D_{#Lambda1}^{WHhdec}", 20, 0, 1),
+        Discriminant("D_0minus_WH_hadronic", "D_{0-}^{WHh}", defaultnbins, 0, 1),
+        Discriminant("D_CP_WH_hadronic", "D_{CP}^{WHh}", defaultnbins, -.1, .1),
+        Discriminant("D_g2_WH_hadronic", "D_{0h+}^{WHh}", defaultnbins, 0, 1),
+        Discriminant("D_g1g2_WH_hadronic", "D_{int}^{WHh}", defaultnbins, -1, .05),
+        Discriminant("D_g1prime2_WH_hadronic", "D_{#Lambda1}^{WHh}", defaultnbins, 0, 1),
+        Discriminant("D_g1g1prime2_WH_hadronic", "D_{#Lambda1int}^{WHh}", defaultnbins, 0, 1),
+        Discriminant("D_0minus_WHdecay_hadronic", "D_{0-}^{WHhdec}", defaultnbins, 0, 1),
+        Discriminant("D_g2_WHdecay_hadronic", "D_{0h+}^{WHhdec}", defaultnbins, 0, 1),
+        Discriminant("D_g1prime2_WHdecay_hadronic", "D_{#Lambda1}^{WHhdec}", defaultnbins, 0, 1),
     ] + [
         Discriminant(
                      "D_g1{}_{}{}_WHdecay_hadronic{}".format(i, gj, 4-i, prime),
                      "D{}^[WHhdec]_[g_[1]^[{}]g_[{}]^[{}]]".format("'" if prime else "", i, gj, 4-i).replace("[", "{").replace("]", "}"),
-                     20,
+                     defaultnbins,
                      *minmax_g1jgik["WHh", gj, i, prime]
                     )
             for prime in ("", "_prime")
--- step6_maketemplates.py
+++ step6_maketemplates.py
@@ -1,4 +1,5 @@
 from helperstuff import config
+from helperstuff.filemanager import KeepWhileOpenFile
 from helperstuff.samples import Sample
 from helperstuff.templates import DataTree, datatrees, TemplatesFile, templatesfiles
 import os
@@ -10,18 +11,17 @@ cmssw = [int(i) for i in os.environ["CMSSW_VERSION"].split("_")[1:]]
 if cmssw[0] == 8:
     raise ValueError("TemplateBuilder does not seem to work in CMSSW_8_X; the templates end up filled with NaNs.  Try CMSSW_7_4_X or CMSSW_7_6_X, I have tested that it works there.")
 
-def touch(filename):
-    open(filename, 'a').close()
-
 def buildtemplates(*args):
     templatesfile = TemplatesFile(*args)
     print templatesfile
     if os.path.exists(templatesfile.templatesfile()):
         return
-    touch(templatesfile.templatesfile())
-    subprocess.call([os.path.join(config.repositorydir, "TemplateBuilder/buildTemplate.exe"), templatesfile.jsonfile()])
-    if not os.path.exists(templatesfile.templatesfile()):
-        raise RuntimeError("Something is wrong!  {} was not created.".format(templatesfile.templatesfile()))
+    with KeepWhileOpenFile(templatesfile.templatesfile() + ".tmp") as f:
+        if f:
+            if not os.path.exists(templatesfile.templatesfile()):
+                subprocess.call([os.path.join(config.repositorydir, "TemplateBuilder/buildTemplate.exe"), templatesfile.jsonfile()])
+            if not os.path.exists(templatesfile.templatesfile()):
+                raise RuntimeError("Something is wrong!  {} was not created.".format(templatesfile.templatesfile()))
 
 def copydata(*args):
     if len(args) == 1 and isinstance(args[0], DataTree):
