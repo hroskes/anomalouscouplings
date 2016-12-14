a079d235da16060c9c1bf5d2682f9fcf4cfa014c
Heshy Roskes <jroskes1@jhu.edu>
2016-12-14 19:38:28 +0100

    ggH is ok...

--- step3_withdiscriminants/deleteme.py
+++ step3_withdiscriminants/deleteme.py
@@ -3,11 +3,13 @@ from helperstuff.samples import *
 from helperstuff.utilities import *
 from collections import Counter
 
+"""
 samples = [Sample("160928", "VBF", hypothesis) for hypothesis in prodonlyhypotheses]
 
 othersamples = [ReweightingSample("VBF", h) for h in "fa2prod-0.5", "fa2dec-0.5", "fa2proddec0.5"]
 
 for s in samples:
+    print
     print s
     print
     f = tfiles[s.withdiscriminantsfile()]
@@ -19,3 +21,17 @@ for s in samples:
                 c[sample] += 1
     for sample in othersamples:
         print sample, c[sample]
+"""
+
+samples = [Sample("160928", "ggH", hypothesis) for hypothesis in decayonlyhypotheses]
+
+for s in samples:
+    print
+    print s
+    f = tfiles[s.withdiscriminantsfile()]
+    t = f.candTree
+    n = 0
+    for entry in t:
+         if 2 / 0.361504459048676 * t.reweightingweights[1] + 2 * t.reweightingweights[0] - t.reweightingweights[2] < 0:
+             n += 1
+    print n
