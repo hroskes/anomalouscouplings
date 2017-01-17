f8f9c5c92f69218a9d80c2a2e6c250cfdff3dc84
Heshy Roskes <jroskes1@jhu.edu>
2017-01-17 22:17:43 +0100

    fix c constant for VHMELA

--- helperstuff/CJLSTscripts/__init__.py
+++ helperstuff/CJLSTscripts/__init__.py
@@ -9,7 +9,7 @@ CJLSTscriptsfile = os.path.join(CJLSTscriptsfolder, "CJLSTscripts.cc")
 #have to be in order of who includes whose header file
 scripts = ["cConstants", "Category", "bitops", "FinalStates"]
 
-downloader = downloadfromCJLST.Downloader("01316520484cc2c15443ab6f3419d59b20b1a715")
+downloader = downloadfromCJLST.Downloader("53c5ec72c985d8a4081753ba65d454ce31f9e8e3")
 for script in scripts:
     downloader.add("AnalysisStep/src/{}.cc".format(script))
     downloader.add("AnalysisStep/interface/{}.h".format(script))
