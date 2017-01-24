621beb71dc7192efffc6fecd3bbd39d494d42834
Heshy Roskes <jroskes1@jhu.edu>
2017-01-24 19:49:39 +0100

    fix compare_POWHEG_JHUGen

--- step3_withdiscriminants/compare_POWHEG_JHUGen.py
+++ step3_withdiscriminants/compare_POWHEG_JHUGen.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python
 
-from helperstuff import style
-from helperstuff.discriminants import discriminant
-from helperstuff.enums import *
-from helperstuff.samples import *
-from helperstuff.utilities import tfiles
+from itertools import product
+import os
+
 import ROOT
 
-discriminants = [discriminant(_) for _ in "D_0minus_VBF", "D_CP_VBF", "D_0minus_HadZH", "D_CP_HadZH"]
+from helperstuff import config, style
+from helperstuff.discriminants import discriminant
+from helperstuff.enums import analyses, categories, Category, hffhypotheses
+from helperstuff.samples import Sample
+from helperstuff.templates import TemplatesFile
+from helperstuff.utilities import tfiles
 
 assert len(config.productionsforcombine) == 1
 production = config.productionsforcombine[0]
-
-def category(discname):
-    if "VBF" in discname: result = Category("VBFtagged").idnumbers
-    if "ZH" in discname: result = Category("VHHadrtagged").idnumbers
-    assert len(result) == 1
-    return result[0]
 
 def samples(productionmode):
   if productionmode in ("HJJ", "ttH"):
@@ -37,32 +34,38 @@ def samples(productionmode):
 
 cache = {}
 
-for discname, title, bins, min, max in discriminants:
-  for productionmode in "HJJ", "ttH", "VBF", "ZH", "WH":
-    hstack = ROOT.THStack(productionmode, title)
-    legend = ROOT.TLegend(.6, .7, .9, .9)
-    legend.SetBorderSize(0)
-    legend.SetFillStyle(0)
-    for color, sample in enumerate(samples(productionmode), start=1):
-      hname = "{}{}{}{}{}".format(discname, sample.productionmode, sample.hypothesis, sample.hffhypothesis, sample.alternategenerator)
-      wt = "({}>-998)*({})*(category=={})".format(discname, sample.weightname(), category(discname))
-      tfiles[sample.withdiscriminantsfile()].candTree.Draw("{}>>{}({},{},{})".format(discname, hname, bins, min, max), wt, "hist")
-      h = cache[discname,sample] = getattr(ROOT, hname)
-      print h
-      h.Scale(1/h.Integral())
-      h.SetLineColor(color)
-      hstack.Add(h)
-      legend.AddEntry(h, str(sample).replace(" "+str(sample.production), ""), "l")
+for analysis, category in product(analyses, categories):
+  if category == "Untagged": continue
+  for discname, title, bins, min, max in TemplatesFile("ggh", "2e2mu", analysis, category, production).discriminants[0:2]:
+    for productionmode in "HJJ", "ttH", "VBF", "ZH", "WH":
+      categoryname = "category_"+analysis.categoryname
+      assert len(category.idnumbers) == 1
+      categoryid = category.idnumbers[0]
+      hstack = ROOT.THStack(productionmode, title)
+      legend = ROOT.TLegend(.6, .7, .9, .9)
+      legend.SetBorderSize(0)
+      legend.SetFillStyle(0)
+      for color, sample in enumerate(samples(productionmode), start=1):
+        if color == 5: color = ROOT.kYellow+3
+        hname = "{}{}{}{}{}{}".format(discname, sample.productionmode, sample.hypothesis, sample.hffhypothesis, sample.alternategenerator, analysis)
+        wt = "({}>-998)*({})*({}=={})".format(discname, sample.weightname(), categoryname, categoryid)
+        tfiles[sample.withdiscriminantsfile()].candTree.Draw("{}>>{}({},{},{})".format(discname, hname, bins, min, max), wt, "hist")
+        h = cache[analysis,discname,sample] = getattr(ROOT, hname)
+        print h
+        h.Scale(1/h.Integral())
+        h.SetLineColor(color)
+        hstack.Add(h)
+        legend.AddEntry(h, str(sample).replace(" "+str(sample.production), ""), "l")
 
-    c = ROOT.TCanvas()
-    hstack.Draw("hist nostack")
-    hstack.GetXaxis().SetTitle(title)
-    legend.Draw()
+      c = ROOT.TCanvas()
+      hstack.Draw("hist nostack")
+      hstack.GetXaxis().SetTitle(title)
+      legend.Draw()
 
-    directory = os.path.join(config.plotsbasedir, "templateprojections", "compare_POWHEG_JHUGen", str(productionmode))
-    try:
-      os.makedirs(directory)
-    except OSError:
-      pass
-    for ext in "png eps root pdf".split():
-      c.SaveAs(os.path.join(directory, "{}_{}.{}".format(discname, productionmode, ext)))
+      directory = os.path.join(config.plotsbasedir, "templateprojections", "compare_POWHEG_JHUGen", str(analysis), str(productionmode))
+      try:
+        os.makedirs(directory)
+      except OSError:
+        pass
+      for ext in "png eps root pdf".split():
+        c.SaveAs(os.path.join(directory, "{}_{}.{}".format(discname, productionmode, ext)))
