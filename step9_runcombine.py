4102108fb6fc04de9c6ca6c799aab95af73292a4
Heshy Roskes <jroskes1@jhu.edu>
2017-01-25 17:17:20 +0100

    they are not nuisance parameters?

--- step9_runcombine.py
+++ step9_runcombine.py
@@ -32,7 +32,7 @@ eval $(scram ru -sh) &&
 combine -M MultiDimFit .oO[workspacefile]Oo. --algo=grid --points .oO[npoints]Oo. \
         --setPhysicsModelParameterRanges CMS_zz4l_fai1=.oO[scanrange]Oo. -m 125 -n $1_.oO[append]Oo..oO[moreappend]Oo. \
         .oO[-t -1]Oo. --setPhysicsModelParameters .oO[setphysicsmodelparameters]Oo. -V -v 3 --saveNLL \
-        -S .oO[usesystematics]Oo. --saveSpecifiedNuis=r_VVH,r_ggH |& tee log.oO[expectfaiappend]Oo..oO[moreappend]Oo...oO[exporobs]Oo.
+        -S .oO[usesystematics]Oo. --saveSpecifiedFunc=r_VVH,r_ggH |& tee log.oO[expectfaiappend]Oo..oO[moreappend]Oo...oO[exporobs]Oo.
 """
 
 def check_call_test(*args, **kwargs):
