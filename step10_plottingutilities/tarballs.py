27bc2248bd7d8ebf706d490a04c19b7fb38d1a25
Heshy Roskes <jroskes1@jhu.edu>
2017-01-25 20:11:26 +0100

    script to make tarball

---
+++ step10_plottingutilities/tarballs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import glob
+import os
+import shutil
+import subprocess
+import tempfile
+
+from helperstuff import config
+from helperstuff.enums import analyses, categories
+
+assert len(config.productionsforcombine) == 1
+production = config.productionsforcombine[0]
+
+tmpdir = tempfile.mkdtemp()
+for analysis in analyses:
+    for category in categories:
+        if category == "Untagged": productionmode = "ggH"
+        if category == "VBFtagged": productionmode = "VBF"
+        if category == "VHHadrtagged": productionmode = "VH"
+        for filename in glob.glob(os.path.join(config.plotsbasedir, "templateprojections", "fullrange", "{}_{}".format(analysis, production), str(category), "2e2mu", productionmode, "*.pdf")):
+             shutil.copy(filename, os.path.join(tmpdir, "{}_{}_{}".format(analysis, category, os.path.basename(filename))))
+
+subprocess.check_call(["tar", "-cvzf", "templateprojections.tar.gz", "-C", tmpdir] + os.listdir(tmpdir))
