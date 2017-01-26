cf96fd0a73d7147e634f373da1f9f45231388b10
Heshy Roskes <jroskes1@jhu.edu>
2017-01-26 21:14:04 +0100

    both options

--- step3_withdiscriminants/mjj.py
+++ step3_withdiscriminants/mjj.py
@@ -8,13 +8,18 @@ from helperstuff.utilities import *
 f = ROOT.TFile("VBF0+170119.root")
 t = f.candTree
 
-h = ROOT.TH1F("h", "h", 100, 40, 220)
-hdelta = ROOT.TH1F("h", "h", 100, -200, 200)
+category = False
+
+h = ROOT.TH1F("h", "h", 100, 0, 7000)
+if category:
+    hdelta = ROOT.TH1F("hdelta", "h", 100, -2300, 300)
+else:
+    hdelta = ROOT.TH1F("hdelta", "h", 100, -5000, 7000)
 
 
 length = t.GetEntries()
 for i, entry in enumerate(t, start=1):
-#    if t.category_0P != VHHadrTaggedIchep16: continue
+    if t.category_0P != VHHadrTaggedIchep16: continue
     jets = [tlvfromptetaphim(pt, eta, phi, m) for pt, eta, phi, m in zip(t.LHEAssociatedParticlePt, t.LHEAssociatedParticleEta, t.LHEAssociatedParticlePhi, t.LHEAssociatedParticleMass)]
     if len(jets) < 2: continue
     mjj = (jets[0]+jets[1]).M()
@@ -23,9 +28,13 @@ for i, entry in enumerate(t, start=1):
     if i % 1000 == 0 or i == length:
        print i, "/", length
 
+append = ""
+if not category:
+    append = "_all"
+
 c = ROOT.TCanvas()
 h.Draw()
-c.SaveAs("~/www/TEST/mjj_lhe_all.png")
+c.SaveAs("~/www/TEST/mjj_lhe{}.png".format(append))
 hdelta.Draw()
-c.SaveAs("~/www/TEST/deltamjj_all.png")
+c.SaveAs("~/www/TEST/deltamjj{}.png".format(append))
 
