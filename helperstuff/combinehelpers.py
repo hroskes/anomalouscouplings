8fe5718079e573fb3dabb46777ab721ad4348538
Heshy Roskes <jroskes1@jhu.edu>
2017-02-01 15:30:36 +0100

    need more time

--- helperstuff/combinehelpers.py
+++ helperstuff/combinehelpers.py
@@ -148,7 +148,7 @@ def gettemplate(*args):
 
     #############
     from datetime import date
-    if date.today() > date(2017, 1, 31):
+    if date.today() > date(2017, 2, 3):
         raise ValueError("VBF bkg 4mu??!")
     if t.channel == "4mu" and t.productionmode == "VBFbkg":
         return gettemplate(t.analysis, t.category, t.productionmode, t.production, "4e")
