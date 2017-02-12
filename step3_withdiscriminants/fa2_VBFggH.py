0218c14449a0b9a3520d8e3c57f92b787f06af2f
Heshy Roskes <jroskes1@jhu.edu>
2017-02-12 20:49:58 +0100

    more plots

--- step3_withdiscriminants/fa2_VBFggH.py
+++ step3_withdiscriminants/fa2_VBFggH.py
@@ -14,12 +14,12 @@ from helperstuff.rootoverloads import histogramadd
 assert len(config.productionsforcombine) == 1
 production = config.productionsforcombine[0]
 
-def makeplot(disc, channel):
+def makeplot(disc, channel, fa2, category):
   lines = (
-           Line(ReweightingSample("ggH", "fa2-0.5"), "ggH", 1, ReweightingSample("ggH", "a2")),
-           Line(ReweightingSample("VBF", "fa2-0.5"), "VBF", 2, ReweightingSample("VBF", "a2")),
-#           Line(ReweightingSample("ZH", "fa2-0.5"), "ZH", 4, ReweightingSample("ZH", "a2")),
-#           Line(ReweightingSample("WH", "fa2-0.5"), "WH", ROOT.kGreen+3, ReweightingSample("WH", "a2")),
+           Line(ReweightingSample("ggH", "fa2{}".format(fa2)), "ggH", 1, ReweightingSample("ggH", "a2")),
+           Line(ReweightingSample("VBF", "fa2{}".format(fa2)), "VBF", 2, ReweightingSample("VBF", "a2")),
+#           Line(ReweightingSample("ZH", "fa2{}".format(fa2)), "ZH", 4, ReweightingSample("ZH", "a2")),
+#           Line(ReweightingSample("WH", "fa2{}".format(fa2)), "WH", ROOT.kGreen+3, ReweightingSample("WH", "a2")),
           )
 
   hs = {}
@@ -31,9 +31,9 @@ def makeplot(disc, channel):
 
   for sample, title, color, reweightfrom in lines:
     for linestyle, cut in enumerate((None, "ZZPt<200", "ZZPt>200"), start=1):
-      color=color*2-2+linestyle
-      if cut is None: continue
-#      if cut is not None: continue
+#      color=color*2-2+linestyle
+#      if cut is None: continue
+      if cut is not None: continue
 
       htotal = hs[disc,sample,style] = None
       for hypothesis in sample.productionmode.generatedhypotheses:
@@ -47,33 +47,38 @@ def makeplot(disc, channel):
           cut=cut,
 #          linestyle=linestyle,
           channel=channel,
-          category="VBFtagged",
+          category=category,
           analysis="fa2",
           bins=20,
         )
         if htotal is None: htotal = h
         else: htotal += h
+        h.Sumw2()
       htotal.Scale(1/htotal.Integral())
       htotal.SetLineWidth(2)
       htotal.SetMarkerStyle(20+linestyle)
       hstack.Add(htotal)
       usetitle = title
       if cut: usetitle += " ("+cut.replace("ZZP", "p")+")"
-      l.AddEntry(htotal, usetitle, "l")
+      l.AddEntry(htotal, usetitle, "lp")
 
   hstack.Draw("PEnostack")
   l.Draw()
 
-  saveasdir = os.path.join(config.plotsbasedir, "fa2_pTeffect", str(channel))
+  categorystr = str(category) if category else ""
+
+  saveasdir = os.path.join(config.plotsbasedir, "fa2_pTeffect", str(channel), categorystr)
   try:
     os.makedirs(saveasdir)
   except OSError:
     pass
   for ext in "png eps root pdf".split():
-    c.SaveAs(os.path.join(saveasdir, "{}.{}".format(disc, ext)))
+    c.SaveAs(os.path.join(saveasdir, "fa2{}_{}.{}".format(fa2, disc, ext)))
 
 if __name__ == "__main__":
   for channel in "2e2mu", "4e", "4mu":
-    makeplot("D_0hplus_decay", channel)
-    makeplot("D_int_decay", channel)
+    for fa2 in -.5, -.9:
+      for category in "Untagged", "VBFtagged", "VHHadrtagged", None:
+        makeplot("D_0hplus_decay", channel, fa2, category)
+        makeplot("D_int_decay", channel, fa2, category)
 
