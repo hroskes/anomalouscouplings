3a70ee8c9327d2cd221d0dfb617e30f29c590372
Heshy Roskes <jroskes1@jhu.edu>
2017-08-29 15:22:31 -0400

    fix

--- helperstuff/copysmoothingparameters.py
+++ helperstuff/copysmoothingparameters.py
@@ -5,11 +5,18 @@ example script to copy smoothing parameters from one set of templates to another
 
 from templates import *
 
+templates = []
+
 for channel in channels:
   for hypothesis in "0+_photoncut", "L1_photoncut", "L1Zg", "fL10.5_photoncut", "fL1Zg0.5", "fL10.5fL1Zg0.5":
-    Template(channel, "ggH", "fL1fL1Zg", "170825", "Untagged", hypothesis).reweightrebin = [0, 1, 2]
-  Template(channel, "qqZZ", "fL1fL1Zg", "170825", "Untagged").reweightrebin = [0, 1, 2]
-  Template(channel, "ggZZ", "fL1fL1Zg", "170825", "Untagged").reweightrebin = [0, 1, 2]
-  Template(channel, "VBF bkg", "fL1fL1Zg", "170825", "Untagged").reweightrebin = [0, 1, 2]
+    templates.append(Template(channel, "ggH", "fL1fL1Zg", "170825", "Untagged", hypothesis))
+  templates.append(Template(channel, "qqZZ", "fL1fL1Zg", "170825", "Untagged"))
+  templates.append(Template(channel, "ggZZ", "fL1fL1Zg", "170825", "Untagged"))
+  templates.append(Template(channel, "VBF bkg", "fL1fL1Zg", "170825", "Untagged"))
+
+
+for template in templates:
+  template.reweightrebin = None
+  template.reweightaxes = [0, 1, 2]
 
 Template.writedict()
