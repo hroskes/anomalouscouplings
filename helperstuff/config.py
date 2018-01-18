11854d62e1200043731adbf10df94ef216145d59
Heshy Roskes <jroskes1@jhu.edu>
2018-01-18 16:55:58 +0100

    actually run on new samples

--- helperstuff/config.py
+++ helperstuff/config.py
@@ -81,7 +81,7 @@ if LHE:
     smearptmuon = 2.169/6
     smearptjet = 18./6
 else:
-    productionsforcombine = ["170825"]
+    productionsforcombine = ["171217"]
 
 if len(productionsforcombine) == 1:
     productionforcombine = productionsforcombine[0]
