8b55a65fb2b9f5ed5ae20038b8b87c49f39cddc2
Heshy Roskes <jroskes1@jhu.edu>
2018-01-25 09:24:06 -0500

    decay, VBF, VH

--- step3_withdiscriminants/test.py
+++ step3_withdiscriminants/test.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 
+assert __name__ == "__main__"
+
+import argparse
+
+p = argparse.ArgumentParser()
+p.add_argument("process", choices="decay VBFdecay HadVHdecay".split())
+args = p.parse_args()
+process = args.process
+
 import glob
 import itertools
 import os
@@ -11,10 +20,7 @@ from helperstuff.copyplots import copyplots
 from helperstuff.discriminants import discriminant
 from helperstuff.treewrapper import TreeWrapper
 
-disc = "D_4couplings_decay"
-
-assert __name__ == "__main__"
-
+disc = "D_4couplings_"+process
 disc = discriminant(disc)
 
 t = ROOT.TChain("candTree")
@@ -24,28 +30,28 @@ for filename in glob.iglob("*.root"):
 
 c = ROOT.TCanvas()
 c.SetLogy()
-t.Draw(disc.name+">>h({},{},{})".format(disc.bins, disc.min, disc.max))
+t.Draw(disc.name+">>h({},{},{})".format(disc.bins, disc.min, disc.max), disc.replace("D_4couplings", "D_0minus">-998))
 h = ROOT.h
 
-numbersofbins = [(variablename, len(binning)+1) for variablename, binning in TreeWrapper.binning_4couplings_decay]
+numbersofbins = [(variablename, len(binning)+1) for variablename, binning in getattr(TreeWrapper, "binning_4couplings_"+process)]
 for binnumber in range(disc.bins):
-  for D_0minus_decay, D_0hplus_decay, D_L1_decay, D_L1Zg_decay, D_int_decay in itertools.product(
-      (0, .5, 1),     (0, .6, 1),     (0, .6, 1), (0, .5, 1),   (-1, 1)
+  for D_0minus, D_0hplus, D_L1, D_L1Zg, D_int in itertools.product(
+      (0, .5, 1), (0, .6, 1), (0, .6, 1), (0, .5, 1), (-1, 1)
   ):
     result = 0
-    for variablename, binning in TreeWrapper.binning_4couplings_decay:
+    for variablename, binning in getattr(TreeWrapper, "binning_4couplings_"+process):
       result *= len(binning)+1
-      variable = globals()[variablename]
+      variable = globals()[variablename.rsplit("_", 1)[0]]
       for bin in binning:
         if variable > bin:
           result += 1
     if result == binnumber:
       if h.GetBinContent(binnumber+1) < 100:
-        print "{:3d} {:4.1f} {:4.1f} {:4.1f} {:4.1f} {:4.1f} {:5d}".format(binnumber, D_0minus_decay, D_0hplus_decay, D_L1_decay, D_L1Zg_decay, D_int_decay, int(h.GetBinContent(binnumber+1)))
+        print "{:3d} {:4.1f} {:4.1f} {:4.1f} {:4.1f} {:4.1f} {:5d}".format(binnumber, D_0minus, D_0hplus, D_L1, D_L1Zg, D_int, int(h.GetBinContent(binnumber+1)))
       break
   else:
     raise ValueError("No bin #{}??".format(binnumber))
 
 for ext in "png eps root pdf".split():
-  c.SaveAs(os.path.join(config.plotsbasedir, "TEST", "test."+ext))
+  c.SaveAs(os.path.join(config.plotsbasedir, "TEST", "D_4couplings_"+process+"."+ext))
 copyplots("TEST")
