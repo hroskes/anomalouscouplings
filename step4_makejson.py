39da59c7ed0bbb08b1b8aea36d8ba83922bda20f
Heshy Roskes <jroskes1@jhu.edu>
2018-03-15 16:39:20 -0400

    remove parallelizing makejson, since invertedmatrix is cached

--- step4_makejson.py
+++ step4_makejson.py
@@ -1,23 +1,16 @@
 #!/usr/bin/env python
-
-import json
-import os
-
 from helperstuff.templates import TemplatesFile, templatesfiles
-from helperstuff.utilities import KeepWhileOpenFile, LSB_JOBID
+import json
 
 def makejson(*args):
-  templatesfile = TemplatesFile(*args)
-  if templatesfile.copyfromothertemplatesfile is not None: return
-  with KeepWhileOpenFile(templatesfile.jsonfile()+".tmp", message=LSB_JOBID()) as kwof:
+    templatesfile = TemplatesFile(*args)
+    if templatesfile.copyfromothertemplatesfile is not None: return
     print templatesfile
-    if not kwof: return
-    if os.path.exists(templatesfile.jsonfile()): return
 
     jsonstring = json.dumps(templatesfile.getjson(), sort_keys=True, indent=4, separators=(',', ': '))
     filename = templatesfile.jsonfile()
     with open(filename, "w") as f:
-      f.write(jsonstring)
+        f.write(jsonstring)
 
 if __name__ == "__main__":
     for templatesfile in templatesfiles:
