e4a408b00db2231fb2d32b27cb94d94aebf0dbfc
Heshy Roskes <jroskes1@jhu.edu>
2018-11-30 10:56:29 -0500

    useDbkgorthogonal when there are bins with 0

--- helperstuff/customsmoothing/redointerference.py
+++ helperstuff/customsmoothing/redointerference.py
@@ -8,24 +8,31 @@ def redointerference(hsmooth, rawprojections, templatesandfactors, mirrorjsn, ne
     if mirrorjsn:
         for key, value in mirrorjsn.iteritems():
             if key == "type":
-                if value != "mirror": raise ValueError("Unknown type {} in mirrorjsn".format(value))
+                if value == "rescale": dorescale = True
+                elif value == "mirror": dorescale = False
+                else: raise ValueError("Unknown type {} in mirrorjsn".format(value))
             elif key == "axis":
                 if value != 1: raise ValueError("Unknown axis {} in mirrorjsn".format(value))
             elif key == "antisymmetric":
                 antisymmetric = value
+            elif key == "factor":
+                rescaleby = value
             else:
                 raise ValueError("Unknown key {} in mirrorjsn".format(key))
 
-        if antisymmetric: sign = -1
-        else: sign = 1
+        if dorescale:
+            hsmooth.Scale(rescaleby)
+        else:
+            if antisymmetric: sign = -1
+            else: sign = 1
 
-        tmp = hsmooth.Clone("tmp"+hsmooth.GetName())
-        for binx in range(tmp.GetNbinsX()):
-            for biny in range(tmp.GetNbinsY()):
-                for binz in range(tmp.GetNbinsZ()):
-                    hsmooth.SetBinContent(binx, biny, binz,
-                                          tmp.GetBinContent(binx, biny, binz)
-                                            + sign*tmp.GetBinContent(binx, tmp.GetNbinsY()-biny+1, binz)
-                                         )
+            tmp = hsmooth.Clone("tmp"+hsmooth.GetName())
+            for binx in range(tmp.GetNbinsX()):
+                for biny in range(tmp.GetNbinsY()):
+                    for binz in range(tmp.GetNbinsZ()):
+                        hsmooth.SetBinContent(binx, biny, binz,
+                                              tmp.GetBinContent(binx, biny, binz)
+                                                + sign*tmp.GetBinContent(binx, tmp.GetNbinsY()-biny+1, binz)
+                                             )
 
     return True #do make new control plots
