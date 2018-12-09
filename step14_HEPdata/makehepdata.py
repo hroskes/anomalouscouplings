b39b8a447154e9a072f9e2cf9b4fa3df954bf201
Heshy Roskes <jroskes1@jhu.edu>
2018-12-09 17:47:01 +0100

    HIG-18-002 onshell

--- step14_HEPdata/makehepdata.py
+++ step14_HEPdata/makehepdata.py
@@ -3,7 +3,7 @@
 import argparse
 if __name__ == "__main__":
   p = argparse.ArgumentParser()
-  p.add_argument("cadi", choices=["HIG-17-011"])
+  p.add_argument("cadi", choices=["HIG-17-011", "HIG-18-002"])
   g = p.add_mutually_exclusive_group()
   g.add_argument("--force", "-f", action="store_true")
   g.add_argument("--check", "-c", action="store_true")
@@ -64,16 +64,28 @@ class PaperInfo(object):
     return self.__cadi
   @property
   def reactions(self):
-    return {"HIG-17-011": ["P P --> HIGGS --> Z0 Z0 --> LEPTON+ LEPTON- LEPTON+ LEPTON-"]}[self.cadi]
+    return {
+      "HIG-17-011": ["P P --> HIGGS --> Z0 Z0 --> LEPTON+ LEPTON- LEPTON+ LEPTON-"],
+      "HIG-18-002": ["P P --> Z0 Z0 --> LEPTON+ LEPTON- LEPTON+ LEPTON-"],
+    }[self.cadi]
   @property
   def sqrts(self):
-    return {"HIG-17-011": [13000]}[self.cadi]
+    return {
+      "HIG-17-011": [13000],
+      "HIG-18-002": [13000],
+    }[self.cadi]
   @property
   def rootfile(self):
-    return {"HIG-17-011": "/afs/cern.ch/user/h/hroskes/www/anomalouscouplings_production/limits/{self.fai}_limit_lumi35.8671.root"}[self.cadi].format(self=self)
+    return {
+      "HIG-17-011": "/afs/cern.ch/user/h/hroskes/www/anomalouscouplings_production/limits/{self.fai}_limit_lumi35.8671.root",
+      "HIG-18-002": "/afs/cern.ch/user/h/hroskes/www/anomalouscouplings_HIG18002/limits/{self.fai}_limit_lumi80.15.root",
+    }[self.cadi].format(self=self)
   @property
   def lumidict(self):
-    return {"HIG-17-011": {7: 5.1, 8: 19.7, 13: 38.6}}[self.cadi]
+    return {
+      "HIG-17-011": {7: 5.1, 8: 19.7, 13: 38.6},
+      "HIG-18-002": {7: 5.1, 8: 19.7, 13: 80.2},
+    }[self.cadi]
 
 class Table(PaperInfo):
   __metaclass__ = abc.ABCMeta
@@ -87,11 +99,23 @@ class Table(PaperInfo):
   def thumbfigurename(self):
     return "thumb_"+self.figurename
   @property
+  def notpublishedyet(self):
+    if self.cadi == "HIG-18-002": return True
+    return False
+  @property
   def figureurl(self):
-    return "http://cms-results.web.cern.ch/cms-results/public-results/publications/"+self.cadi+"/"+self.figurename
+    if self.notpublishedyet:
+      result = "file:/afs/cern.ch/work/h/hroskes/AN/papers/{self.cadi}/trunk/Figure_{self.figurenumber:03d}".format(self=self)
+      if self.figureletter is not None: result += "-"+self.figureletter
+      result += ".pdf"
+      return result
+    return "http://cms-results.web.cern.ch/cms-results/public-results/publications/{self.cadi}/{self.figurename}".format(self=self)
   @property
   def location(self):
-    return "Fig. {:d}{} on page {:d} of the paper".format(self.figurenumber, self.figureletter if self.figureletter else None, self.figurepage)
+    if self.notpublishedyet:
+      return "Fig. {:d}{} of the paper".format(self.figurenumber, self.figureletter if self.figureletter else None)
+    else:
+      return "Fig. {:d}{} on page {:d} of the paper".format(self.figurenumber, self.figureletter if self.figureletter else None, self.figurepage)
   @abc.abstractproperty
   def figurepage(self): pass
   @property
@@ -131,12 +155,24 @@ class Table(PaperInfo):
   def yamlfile(self):
     return self.name.replace(" ", "")
   def getplots(self):
-    try:
-      with open(self.figurename, "w") as newf, contextlib.closing(urllib2.urlopen(self.figureurl)) as oldf:
-        shutil.copyfileobj(oldf, newf)
-    except urllib2.HTTPError:
-      print self.figureurl
-      raise
+    figureurl = self.figureurl
+    if figureurl.endswith(".pdf"):
+      try:
+        with tempfile.NamedTemporaryFile(suffix=".pdf", bufsize=0) as newf, contextlib.closing(urllib2.urlopen(figureurl)) as oldf:
+          shutil.copyfileobj(oldf, newf)
+          subprocess.check_call(["convert", "-density", "150", "-trim", newf.name, "-quality", "100", self.figurename])
+      except urllib2.HTTPError:
+        print self.figureurl
+        raise
+    elif figureurl.endswith(".png"):
+      try:
+        with open(self.figurename, "w") as newf, contextlib.closing(urllib2.urlopen(figureurl)) as oldf:
+          shutil.copyfileobj(oldf, newf)
+      except urllib2.HTTPError:
+        print self.figureurl
+        raise
+    else:
+      assert False, self.figureurl
     subprocess.check_call(["convert", "-thumbnail", "200", self.figurename, self.thumbfigurename])
     assert os.path.exists(self.thumbfigurename)
   @abc.abstractproperty
@@ -161,6 +197,11 @@ class FaiLimitTable(Table):
   @property
   def observables(self):
     return ["SIG/SIG"]
+
+  @abc.abstractmethod
+  def getlegendfromrootfile(self, tfile):
+    pass
+
   @property
   def yaml(self):
     import ROOT
@@ -169,12 +210,7 @@ class FaiLimitTable(Table):
     result = {"name": self.yamlfile, "independent_variables": [x], "dependent_variables": ys}
     xvalues = None
     f = ROOT.TFile(self.rootfile)
-    canvas = f.GetListOfKeys()
-    assert len(canvas) == 1
-    canvas = canvas[0]
-    canvas = canvas.ReadObj()
-    pad = canvas.GetListOfPrimitives()[0]
-    legend = pad.GetListOfPrimitives()[-1]
+    legend = self.getlegendfromrootfile(f)
     for entry in self.legendentries(legend):
       if xvalues is None:
         xvalues = entry.xvalues
@@ -199,11 +235,17 @@ class FaiLimitTable(Table):
       if self.fai == "fa2": return {0.007500944659113884, 0.012208748608827591, -6.613622982598599e-09, 3.1140121592443393e-09, 0.0242480281740427, 0.012350913137197495, 1.719479225670284e-09, -5.649837819809989e-10}
       if self.fai == "fL1": return {0.0001509769936092198, 9.811160452954937e-06, 0.00015089577937033027, 7.448184291547477e-10, 6.218647467903793e-05, 6.269038567552343e-05, 6.28244652034482e-06, 9.799659892451018e-06}
       if self.fai == "fL1Zg": return {0.00014273269334807992, -0.00011004077532561496, -1.194288233818952e-05, -9.83606241788948e-06, 0.00010419703176012263, 1.7338144971290603e-05, -0.00010066330287372693, 8.42108711367473e-05}
+    if self.cadi == "HIG-18-002":
+      if self.fai == "fa3": return {-3.469446951953614e-17, -5.048824459663592e-05, -7.212763011921197e-05, -4.85241471324116e-05, -5.592270326815196e-07, 0.1599999964237213, -0.1599999964237213}
+      if self.fai == "fa2": return {0.0003515805583447218, -1.5517587215185813e-12, -2.256646258746997e-10, 0.019600000232458115, 0.00041023208177648485, 1.6157702702912502e-05, 6.066570676921401e-07, -2.392181785992875e-09, 2.2096777740898688e-09, 0.5600000023841858, 0.01600000075995922}
+      if self.fai == "fL1": return {3.816104435827583e-05, 9.086481567166516e-15, -0.3400000035762787, -0.6800000071525574, -0.3199999928474426, 4.4846903620054945e-05, 6.056595225345518e-10, 6.253050059967791e-07, 1.2750886526191607e-05, -5.451978868364904e-10, 2.5288371396925413e-10}
+      if self.fai == "fL1Zg": return {0.7200000286102295, 0.7799999713897705, 5.376144329716226e-08, -0.3799999952316284, -0.0002512808714527637, 3.570632856053635e-08, -0.0002523555886000395, -0.8799999952316284, 1.2302773484407226e-06, 2.031392796197906e-05, -0.00026075352798216045, 0.800000011920929, -0.699999988079071, -0.0002693945134524256}
     return set()
 
   @property
   def figurenumber(self):
     if self.cadi == "HIG-17-011": return 3
+    if self.cadi == "HIG-18-002": return 4
     assert False
   @property
   def figureletter(self):
@@ -211,12 +253,44 @@ class FaiLimitTable(Table):
 
   @property
   def description(self):
-    return "Observed and expected likelihood scans ${self.fancyfai}\cos{self.fancyphiai}$.  See Section 2 of the paper for more details.".format(self=self)
+    return "Observed and expected likelihood scans of ${self.fancyfai}\cos{self.fancyphiai}$.  See Section 2 of the paper for more details.".format(self=self)
 
   @property
   def figurepage(self):
     if self.cadi == "HIG-17-011": return 6
     assert False
+
+class FaiLimitTable_TwoPartPlot(FaiLimitTable):
+  def getlegendfromrootfile(self, tfile):
+    import ROOT
+    canvas = tfile.GetListOfKeys()
+    assert len(canvas) == 1
+    canvas = canvas[0]
+    canvas = canvas.ReadObj()
+    pad = canvas.GetListOfPrimitives()[0]
+    legend = pad.GetListOfPrimitives()[-1]
+    assert isinstance(legend, ROOT.TLegend), legend
+    return legend
+
+class FaiLimitTable_SixPartPlot(FaiLimitTable):
+  def getlegendfromrootfile(self, tfile):
+    import ROOT
+    canvas = tfile.GetListOfKeys()
+    assert len(canvas) == 1
+    canvas = canvas[0]
+    canvas = canvas.ReadObj()
+    legend = canvas.GetListOfPrimitives()[7]
+    assert isinstance(legend, ROOT.TLegend), legend
+    return legend
+
+class FaiLimitTable_HIG18002(FaiLimitTable_SixPartPlot):
+  @property
+  def name(self):
+    return self.fai+"_onshell.yaml"
+
+  @property
+  def description(self):
+    return "Observed and expected likelihood scans of ${self.fancyfai}\cos{self.fancyphiai}$ using on-shell events only.  See Section 2 of the paper for more details.".format(self=self)
 
 class FaiLegendEntry(object):
   def __init__(self, entry, lumidict, fancyphiai, removepoints):
@@ -259,7 +333,16 @@ class HIG17011(HEPData):
   @property
   def tables(self):
     for fai in "fa3", "fa2", "fL1", "fL1Zg":
-      yield FaiLimitTable(self.cadi, fai)
+      yield FaiLimitTable_TwoPartPlot(self.cadi, fai)
+
+class HIG18002(HEPData):
+  @property
+  def cadi(self):
+    return "HIG-18-002"
+  @property
+  def tables(self):
+    for fai in "fa3", "fa2", "fL1", "fL1Zg":
+      yield FaiLimitTable_HIG18002(self.cadi, fai)
 
 @contextlib.contextmanager
 def cd(newdir):
