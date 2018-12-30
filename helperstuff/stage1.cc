d0aba110e55506260e6419bd804f4d8ee5bfa517
Heshy Roskes <jroskes1@jhu.edu>
2018-12-30 22:05:56 +0100

    commit code from Meng

---
+++ helperstuff/stage1.cc
@@ -0,0 +1,162 @@
+// stage1_reco:
+// ggH_0J =0
+// ggH_1J_PTH_0_60 =1
+// ggH_1J_PTH_60_120 =2
+// ggH_1J_PTH_120_200=3
+// ggH_1J_PTH_200=4
+// ggH_2J_PTH_0_60 =5
+// ggH_2J_PTH_60_120 =6
+// ggH_2J_PTH_120_200=7
+// ggH_2J_PTH_200=8
+// VBF_VBF = 9
+// VBF_PTJET1_GT200 = 10
+// VH_lep =11
+// VH2JET = 12
+// ttH_Lep = 13
+// ttH_Hap = 14
+// VBF_1j = 15;
+// VBF_2j = 16;
+// VBF_REST = ?
+
+float bins_hpt4[]={0,60,120,200};
+TH1F *hpt_bin=new TH1F("hpt_bin","",3, bins_hpt4);
+
+int stage1_reco(int Njets, float pTj1, float mjj, float deta_jj, float H_pt, string &reco_catName,int category, float D1jet){
+	if(category>2 ){
+		switch (category){
+			case 3: reco_catName = "VH_Lep"; break; 
+			case 4: reco_catName = "VH_Had"; break; 
+			case 6: reco_catName = "TTH_Had"; break; 
+			case 5: reco_catName = "TTH_Lep"; break; 
+		}
+		return  8+category; 
+	}
+	else if(category!=0){
+		int vbfTopo=0;
+		if (Njets<2) vbfTopo=0; 
+		vbfTopo = mjj > 400.0 && deta_jj > 2.8;
+
+		//if (pTj1>200 && H_pt<200)   {reco_catName = "VBF_ptj_GT200"; return 10;}
+		if (pTj1>200 )   {reco_catName = "VBF_ptj_GT200"; return 10;}
+		else if(vbfTopo) 	{reco_catName = "VBF_VBF"; return 9;}
+		else {
+			if(category==1){
+				if(D1jet>0.7)
+				{reco_catName = "VBF_1j"; return 15;}
+				else
+					goto lineuntag;
+			}
+			else if (category==2)
+			{reco_catName = "VBF_2j"; return 16;}
+
+		}
+	}
+	else{
+		if (Njets==0)        {reco_catName = "ggH_0j"; return 0;}
+		else if (Njets==1)   {
+lineuntag:
+			int binpt = hpt_bin->FindBin(H_pt);
+			switch(binpt){
+				case 1: reco_catName = "ggH_1j_0_60"; break;
+				case 2: reco_catName = "ggH_1j_60_120"; break;
+				case 3: reco_catName = "ggH_1j_120_200"; break;
+				case 4: reco_catName = "ggH_1j_GT200"; break;
+			}
+			return binpt;
+		} 
+		else if (Njets>=2) {
+			if ( 60 < mjj && mjj < 120 ) {reco_catName = "VH_Had";return 12;}
+			int binpt = hpt_bin->FindBin(H_pt);
+			switch(binpt){
+				case 1: reco_catName = "ggH_2j_0_60"; break;
+				case 2: reco_catName = "ggH_2j_60_120"; break;
+				case 3: reco_catName = "ggH_2j_120_200"; break;
+				case 4: reco_catName = "ggH_2j_GT200"; break;
+			}
+			return binpt+4;
+		}
+	}
+}
+
+int stage1_reco_stage1(int Njets, float pTj1, float mjj, float deta_jj, float H_pt, string &reco_catName,int category){
+	if(category>2 && category!=4){
+		switch (category){
+			case 3: reco_catName = "VH_Lep"; break; 
+//			case 4: reco_catName = "VH_Had"; break; 
+			case 6: reco_catName = "TTH_Had"; break; 
+			case 5: reco_catName = "TTH_Lep"; break; 
+		}
+		return  8+category; 
+	}
+	else{ 
+		int vbfTopo=0;
+		if (Njets<2) vbfTopo=0; 
+		vbfTopo = mjj > 400.0 && deta_jj > 2.8;
+
+		if (pTj1>200 && H_pt<200)   {reco_catName = "VBF_ptj_GT200"; return 10;}
+		//if (pTj1>200 )   {reco_catName = "VBF_ptj_GT200"; return 10;}
+		else if(vbfTopo) 	{reco_catName = "VBF_VBF"; return 9;}
+		if (Njets==0)        {reco_catName = "ggH_0j"; return 0;}
+		else if (Njets==1)   {
+			int binpt = hpt_bin->FindBin(H_pt);
+			switch(binpt){
+				case 1: reco_catName = "ggH_1j_0_60"; break;
+				case 2: reco_catName = "ggH_1j_60_120"; break;
+				case 3: reco_catName = "ggH_1j_120_200"; break;
+				case 4: reco_catName = "ggH_1j_GT200"; break;
+			}
+			return binpt;
+		} 
+		else if (Njets>=2) {
+			if ( 60 < mjj && mjj < 120 ) {reco_catName = "VH_Had";return 12;}
+			int binpt = hpt_bin->FindBin(H_pt);
+			switch(binpt){
+				case 1: reco_catName = "ggH_2j_0_60"; break;
+				case 2: reco_catName = "ggH_2j_60_120"; break;
+				case 3: reco_catName = "ggH_2j_120_200"; break;
+				case 4: reco_catName = "ggH_2j_GT200"; break;
+			}
+			return binpt+4;
+		}
+	}
+}
+int convertCat (int htxs_stage1_red_cat){
+	switch (htxs_stage1_red_cat){
+		case 101: return 16; //ggH_VBF
+		case 103: return 0; //ggH_0j 
+		case 104: return 1;// ggH_1j_0_60 
+		case 105: return 2;// ggH_1j_60_120
+		case 106: return 3; // ggH_1j_120_200 
+		case 107: return 4; //ggH_1j_200 
+		case 108: return 5; //ggH_2j_0_60 
+		case 109: return 6; //ggH_2j_60_120 
+		case 110: return 7; //ggH_2j_120_200
+		case 111: return 8;  //ggH_2j_200
+		case 201: return 9; //VBF_VBF
+		case 205: return 10; //VBF_jpt200
+		case 301: return 11; //VH lep
+		case 203: return 12; //VH Had
+		case 601: return 13; //ttH
+		case 204: return 15; //VBF rest
+		case -1: return -1; 
+		case -2: return -2; 
+
+	}
+}
+
+TString convertsys(TString inputname){
+	if(inputname.Contains("ggH"))
+		return "ggH";
+	else if (inputname.Contains("Wminus")||inputname.Contains("Wplus"))
+		return "WH";
+	else if (inputname.Contains("ZH"))
+		return "ZH";
+	else if (inputname.Contains("VBF"))
+		return "VBF";
+	else if (inputname.Contains("ttH"))
+		return "ttH";
+	else if (inputname.Contains("bbH"))
+		return "bbH";
+	else if (inputname.Contains("ZZTo"))
+		return "qqZZ";
+}
