1a00b8ad02a32adb90bb71e308f55307d7a453db
skyriacoCMS <savvasphy@gmail.com>
2019-06-19 15:57:03 -0400

    Enum change

--- helperstuff/enums.py
+++ helperstuff/enums.py
@@ -704,18 +704,32 @@ class Analysis(MyEnum):
         return False
     @property
     def doGEN(self):
-        if self == "fa3_multiparameter": return False
-        if self == "fa3_multiparameter_nodbkg": return False
-        if self == "fa3_only6bins": return False
-        if self == "fa3_onlyDCP": return False
-        if self == "fa3_STXS": return False
-        if self == "fa3_onlyDbkg": return False
-        if self in ("fa2", "fa3", "fL1", "fL1Zg"): return False
-        if self.isfL1fL1Zg: return False
-        if self == "fa3fa2fL1fL1Zg": return True
-        if self == "fa3fa2fL1fL1Zg_decay": return True
-        if self == "fa3fa2fL1fL1Zg_only6bins": return True
-        if self == "fa3fa2fL1fL1Zg_STXS": return True
+        if config.name ==  "heshy" : 
+            if self == "fa3_multiparameter": return False
+            if self == "fa3_multiparameter_nodbkg": return False
+            if self == "fa3_only6bins": return False
+            if self == "fa3_onlyDCP": return False
+            if self == "fa3_STXS": return False
+            if self == "fa3_onlyDbkg": return False
+            if self in ("fa2", "fa3", "fL1", "fL1Zg"): return False
+            if self.isfL1fL1Zg: return False
+            if self == "fa3fa2fL1fL1Zg": return True
+            if self == "fa3fa2fL1fL1Zg_decay": return True
+            if self == "fa3fa2fL1fL1Zg_only6bins": return True
+            if self == "fa3fa2fL1fL1Zg_STXS": return True
+        if config.name ==  "savvas" : 
+            if self == "fa3_multiparameter": return True
+            if self == "fa3_multiparameter_nodbkg": return False
+            if self == "fa3_only6bins": return False
+            if self == "fa3_onlyDCP": return False
+            if self == "fa3_STXS": return True
+            if self == "fa3_onlyDbkg": return False
+            if self in ("fa2", "fa3", "fL1", "fL1Zg"): return False
+            if self.isfL1fL1Zg: return False
+            if self == "fa3fa2fL1fL1Zg": return False
+            if self == "fa3fa2fL1fL1Zg_decay": return False
+            if self == "fa3fa2fL1fL1Zg_only6bins": return False
+            if self == "fa3fa2fL1fL1Zg_STXS": return False
         assert False, self
     @property
     def doCMS(self):
