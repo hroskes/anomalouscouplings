7c65bf648e1f42e8b23df4b85429597e8e5dfd04
Heshy Roskes <jroskes1@jhu.edu>
2019-07-16 15:46:44 -0400

    including VBF

--- step10_plottingutilities/mergeidenticalscans.py
+++ step10_plottingutilities/mergeidenticalscans.py
@@ -167,12 +167,12 @@ if __name__ == "__main__":
 
   with pc:
     mergeidenticalscans(
-      os.path.join(plotsbasedir, "limits/fa3fa2fL1fL1Zg_decay_CMSfirsttry/limit_lumi137.10_Untagged_scan"+args.fai+"_merged"),
+      os.path.join(plotsbasedir, "limits/fa3fa2fL1fL1Zg_CMSfirsttry/limit_lumi137.10_scan"+args.fai+"_101,-1.0,1.0_101,-0.02,0.02_merged"),
       *reglob(
-         os.path.join(plotsbasedir, "limits/fa3fa2fL1fL1Zg_decay_CMSfirsttry/"),
-         "limit_lumi137.10_Untagged_scan"+args.fai+"(_(f(a1|a3|a2|L1|L1Zg),){4}(f(a1|a3|a2|L1|L1Zg))|_fixothers|)(_(CMS_zz4l_fai?[0-9]_relative=[0-9.-]*,?)*)?.root",
+         os.path.join(plotsbasedir, "limits/fa3fa2fL1fL1Zg_CMSfirsttry/"),
+         "limit_lumi137.10_scan"+args.fai+"(_(f(a1|a3|a2|L1|L1Zg),){4}(f(a1|a3|a2|L1|L1Zg))|_fixothers|)(_(CMS_zz4l_fai?[0-9]_relative=[0-9.-]*,?)*)?_101,-1.0,1.0_101,-0.02,0.02.root",
       ) + reglob(
-         os.path.join(plotsbasedir, "limits/fa3fa2fL1fL1Zg_decay_CMSfirsttry/gridscan/"),
-         "limit_lumi137.10_Untagged_scan"+args.fai+"(_(f(a1|a3|a2|L1|L1Zg),){4}(f(a1|a3|a2|L1|L1Zg))|_fixothers|)(_(CMS_zz4l_fai?[0-9]_relative=[0-9.-]*,?)*)?.root",
+         os.path.join(plotsbasedir, "limits/fa3fa2fL1fL1Zg_CMSfirsttry/gridscan/"),
+         "limit_lumi137.10_scan"+args.fai+"(_(f(a1|a3|a2|L1|L1Zg),){4}(f(a1|a3|a2|L1|L1Zg))|_fixothers|)(_(CMS_zz4l_fai?[0-9]_relative=[0-9.-]*,?)*)?_101,-1.0,1.0_101,-0.02,0.02.root",
       )
     )
