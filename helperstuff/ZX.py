e7c7b0548f1f29cb08669b7dd97330010db049bb
Heshy Roskes <jroskes1@jhu.edu>
2019-09-11 13:00:25 -0400

    new ZX yields

--- helperstuff/ZX.py
+++ helperstuff/ZX.py
@@ -2,7 +2,7 @@ import os
 
 import ROOT
 
-from utilities import KeyDefaultDict
+from utilities import cache, KeyDefaultDict
 from CJLSTscripts import CJLSTscriptsfolder, convertTGraphstoTH1Fs
 
 class FakeRates(ROOT.FakeRates):
@@ -50,18 +50,69 @@ class normalizeZX(object):
     (-mu, el): 1.0049,
   }
 
-  ratio_combination_over_SS_newobjects = {
-    (2016, -mu*mu): 15.1192528623654 / 13.5568,
-    (2016, -el*el): 15.8741490531714 / 13.0417,
-    (2016, -el*mu): 33.6281681155334 / 27.8966,
-    (2017, -mu*mu): 20.1216409539999 / 17.4629,
-    (2017, -el*el): 12.7202599310126 / 10.8171,
-    (2017, -el*mu): 32.2092377174772 / 28.2197,
-    (2018, -mu*mu): 36.0645009910435 / 33.7859,
-    (2018, -el*el): 19.5751513711866 / 16.215,
-    (2018, -el*mu): 53.8482753544511 / 47.9655,
-  }
+  @classmethod
+  @cache
+  def ratio_combination_over_SS_newobjects(cls, year, flavor):
+    lines = iter(cls.txtfile.split("\n"))
+    for line in lines:
+      if line.startswith(str(year)): break
+    for line in lines:
+      if line.startswith("201"): assert False
+      if line.startswith({-el*el: "4e", -mu*mu: "4mu", -el*mu: "2e2mu_Combined"}[flavor]):
+        SS = float(line.split()[1])
+        break
+    for line in lines:
+      if line.startswith("201"): assert False
+      if line.startswith("SS-OS Combination"): break
+    for line in lines:
+      if line.startswith("201"): assert False
+      if line.startswith({-el*el: "4e", -mu*mu: "4mu", -el*mu: "2e2mu"}[flavor]):
+        combination = float(line.split()[1])
+        break
+    return combination / SS
 
   def __new__(cls, year, uselegacyobjects, Z1Flav, Z2Flav):
-    if uselegacyobjects: return cls.ratio_combination_over_SS_newobjects[year, Z1Flav*Z2Flav]
+    if uselegacyobjects: return cls.ratio_combination_over_SS_newobjects(year, Z1Flav*Z2Flav)
     return cls.cb_SS[year, Z1Flav*Z2Flav] * cls.fs_ROS_SS[Z1Flav, Z2Flav]
+
+  txtfile = """
+******************************************************************************
+2016		SS		OS	
+4mu		26.5 +/- 8.1	25.7 +/- 8.2
+4e		13.1 +/- 5.5	20.2 +/- 6.2
+2e2mu		21.6 +/- 6.6	23.6 +/- 7.5
+2mu2e		16.8 +/- 7.0	23.5 +/- 7.2
+2e2mu_Combined	38.5 +/- 9.7	47.1 +/- 10.3
+				
+SS-OS Combination	Yield	
+4mu			26.1	
+4e			16.2	
+2e2mu			42.5	
+				
+******************************************************************************
+2017		SS		OS	
+4mu		30.0 +/- 9.2	31.9 +/- 10.0
+4e		10.8 +/- 4.1	16.1 +/- 4.9
+2e2mu		23.2 +/- 7.1	22.3 +/- 7.1
+2mu2e		14.7 +/- 5.5	20.9 +/- 6.4
+2e2mu_Combined 	37.9 +/- 9.0	43.1 +/- 9.5
+				
+SS-OS Combination	Yield	
+4mu			30.9	
+4e			13.0	
+2e2mu			40.4	
+
+******************************************************************************
+2018		SS		OS		
+4mu		49.9 +/- 15.2	47.7 +/- 14.8
+4e		16.2 +/- 5.9	25.4 +/- 7.7
+2e2mu		35.9 +/- 10.9	33.7 +/- 10.6
+2mu2e		23.6 +/- 8.5	33.5 +/- 10.1
+2e2mu_Combined	59.5 +/- 13.8	67.2 +/- 14.6
+				
+SS-OS Combination	Yield	
+4mu			48.8	
+4e			19.6	
+2e2mu			63.1	
+******************************************************************************
+  """
