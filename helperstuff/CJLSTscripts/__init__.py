77bcd1f2ab6fef608623de6071ff41cf0a47cc4e
Heshy Roskes <jroskes1@jhu.edu>
2019-09-16 17:12:00 -0400

    update Z+X

--- helperstuff/CJLSTscripts/__init__.py
+++ helperstuff/CJLSTscripts/__init__.py
@@ -8,7 +8,7 @@ CJLSTscriptsfolder = os.path.dirname(os.path.abspath(__file__))
 #have to be in order of who includes whose header file
 scripts = ["cConstants", "Discriminants", "Category", "bitops", "FinalStates"]
 
-downloader = downloadfromCJLST.Downloader("dfce25c6d316ae9cb905f9414c9b7202c0f5a98e")
+downloader = downloadfromCJLST.Downloader("7f4ba82b5f36db82350d0f64ba48b85d56067709")
 for script in scripts:
     downloader.add("AnalysisStep/src/{}.cc".format(script))
     downloader.add("AnalysisStep/interface/{}.h".format(script))
